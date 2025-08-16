fe1593d17a3d082c5257946deebf2cbb82fcbc00
Mishree_14 <mishreekalaria14@gmail.com>
2025-08-16 14:47:21 +0530

    Create Printing_strng_using_pointer.cpp

---
+++ Printing_strng_using_pointer.cpp
@@ -0,0 +1,23 @@
+//Mishree Kalaria
+//24070123052
+//Batch:A2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[] = "Mishree";
+    
+    char *ptr = str;
+    
+    while(*ptr !='\0'){
+        cout<<*ptr;
+        ptr++;
+    }
+
+    return 0;
+}
+
+## SAMPLE OUTPUT
+
+Mishree
